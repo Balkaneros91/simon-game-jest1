cca69d2f1d94f1540ca3fef38a1b48a93c85c41c
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-02-09 16:51:20 +0000

    Add

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -43,7 +43,7 @@ describe('game object contains correct keys', () => {
         expect("lastButton" in game).toBe(true);
     });
     test("turnInProgress key exists", () => {
-        exppect("turnInProgress" in game ).toBe(true);
+        expect("turnInProgress" in game ).toBe(true);
     });
     test("turnInProgress key value is false", () => {
         expect("turnInProgress" in game).toBe(true);
